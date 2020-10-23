3dd5cc56562378880bc4d8975d81bed7d2bc7770
dannyelgjl <dannyelgjl@gmail.com>
2020-10-23 02:11:46 -0300

    alterações na action e reducer

--- src/Components/Catalog.tsx
+++ src/Components/Catalog.tsx
@@ -1,15 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import api from '../services/api';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { IProduct } from '../store/modules/cart/types';
 
+import { addProductToCart } from '../store/modules/cart/actions';
+
+import api from '../services/api';
+
+
+
 const Catalog: React.FC = () => {
+  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
     api.get('products').then(response => {
       setCatalog(response.data);
     })
-  }, [])
+  }, []);
+
+  const handleAddProductToCart = useCallback((product: IProduct) => {
+    dispatch(addProductToCart(product));
+  }, [dispatch]);
 
 
 
@@ -21,7 +32,12 @@ const Catalog: React.FC = () => {
         <article key={product.id}>
           <strong>{product.title}</strong>
           <strong>{product.price}</strong>
-          <button type="button">Comprar</button>
+          <button
+            type="button"
+            onClick={() => handleAddProductToCart(product)}
+          >
+            Comprar
+          </button>
         </article>
       ))}
     </main>
--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -5,8 +5,10 @@ const INITITAL_STATE: ICartState = {
   items: []
 }
 
-const cart: Reducer<ICartState> = () => {
+const cart: Reducer<ICartState> = (state, action) => {
+  console.log(state, action);
   return INITITAL_STATE;
+
 }
 
 export default cart;
