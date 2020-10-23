991590cb73aee4042b811a83563ea3538267c5a1
dannyelgjl <dannyelgjl@gmail.com>
2020-10-23 01:48:36 -0300

    definindo valores iniciais no reducer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,5 +1,12 @@
-function cart() {
-  return [];
+import { Reducer } from 'redux';
+import { ICartState } from './types';
+
+const INITITAL_STATE: ICartState = {
+  items: []
+}
+
+const cart: Reducer<ICartState> = () => {
+  return INITITAL_STATE;
 }
 
 export default cart;
