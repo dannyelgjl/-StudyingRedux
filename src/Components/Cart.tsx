3dcabb0b8c87edba193a02ccf3130fd857b315f0
dannyelgjl <dannyelgjl@gmail.com>
2020-10-23 02:31:09 -0300

    compartilhando informações entre os componentes

--- src/Components/Cart.tsx
+++ src/Components/Cart.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { IState } from '../store/index';
+import { ICartItem } from '../store/modules/cart/types';
 
 // import { Container } from './styles';
 
 const Cart: React.FC = () => {
-  const state = useSelector(state => state);
-
-  console.log(state);
+  const cart = useSelector<IState, ICartItem[]>(state => state.cart.items);
 
   return (
     <table>
@@ -19,7 +19,14 @@ const Cart: React.FC = () => {
         </tr>
       </thead>
       <tbody>
-
+        {cart.map(item => (
+          <tr key={item.product.id} >
+            <td>{item.product.title}</td>
+            <td>{item.product.price}</td>
+            <td>{item.quantity}</td>
+            <td>{(item.product.price * item.quantity).toFixed(2)}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
