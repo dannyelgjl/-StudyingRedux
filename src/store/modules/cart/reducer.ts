5ff771e8b8f547c83e6c31a1e1852d3d4212a654
dannyelgjl <dannyelgjl@gmail.com>
2020-10-23 02:34:31 -0300

    aplicando immer para facilitar o trabalho com os estatos imutaveis

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,16 +1,23 @@
 import { Reducer } from 'redux';
 import { ICartState } from './types';
+import produce from 'immer';
 
 const INITITAL_STATE: ICartState = {
   items: []
-}
+};
 
 const cart: Reducer<ICartState> = (state = INITITAL_STATE, action) => {
   switch(action.type) {
     case 'ADD_PRODUCT_TO_CART': {
       const { product } = action.payload;
 
-      return {
+      return produce(state, draft => {
+        draft.items.push({
+          product,
+          quantity: 1,
+        })
+      });      
+     /*** return {
         ...state,
         items: [
           ...state.items,
@@ -19,7 +26,7 @@ const cart: Reducer<ICartState> = (state = INITITAL_STATE, action) => {
             quantity: 1,
           }
         ]
-      };
+      };*/ 
     }
     default : {
       return state;
