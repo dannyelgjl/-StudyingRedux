81911849df7736e7bde7c7beff0c92942a21be08
dannyelgjl <dannyelgjl@gmail.com>
2020-10-23 01:38:07 -0300

    criando modules e rootReduces

--- src/Components/Catalog.tsx
+++ src/Components/Catalog.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-
-
+import { useSelector } from 'react-redux';
 
 const Catalog: React.FC = () => {
+  const catalog = useSelector(state => state);
+
+  console.log(catalog);
+
   return (
     <h1>Catalog</h1>
   );
