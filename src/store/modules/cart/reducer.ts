f7e438b3df66ef8935aeb4cf26aa3a0811015a46
dannyelgjl <dannyelgjl@gmail.com>
2020-10-26 14:32:11 -0300

    finalizando checagem de estoque

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -26,6 +26,11 @@ const cart: Reducer<ICartState> = (state = INITITAL_STATE, action) => {
           }
         break;
       }
+      case 'ADD_PRODUCT_TO_CART_FAILURE': {
+        console.log('Failure', action.payload);
+
+        break;
+      }
       default : {
         return draft;
       }
--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,19 +1,32 @@
-import { all, takeLatest, select } from 'redux-saga/effects';
+import { all, takeLatest, select, call, put } from 'redux-saga/effects';
 import { IState } from '../..';
-import { addProductToCartRequest } from './actions';
+import { addProductToCartFailure, addProductToCartRequest, addProductToCartSuccess } from './actions';
+import api from "../../../services/api";
+import { AxiosResponse } from 'axios';
 
 type CheckProductStockRequest = ReturnType<typeof addProductToCartRequest>
+
+interface IStockResponse {
+  id: number;
+  quantity: number;
+}
 
 function* checkProductStock({ payload }: CheckProductStockRequest) {
   const { product } = payload;
 
   const currentQuantity: number = yield select((state: IState) => {
     return state.cart.items.find(item => item.product.id === product.id)?.quantity ?? 0;
-  });  
+  });
+  
+  const availableStockResponse: AxiosResponse<IStockResponse> = yield call(api.get, `stock/${product.id}`);
 
-  console.log(currentQuantity);
+  if (availableStockResponse.data.quantity > currentQuantity) {
+    yield put(addProductToCartSuccess(product));
+  } else {
+    yield put(addProductToCartFailure(product.id));
+  }
 
-  console.log('add ao carrinho');
+  
 }
 
 export default all([
